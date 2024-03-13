d2762074154908f42a200f774a2ad6436d253ad8
Will Browning <wdbrwnng@memphis.edu>
2024-03-13 14:18:26 -0500

    Fixed issues with Problem 7

--- problem7.rb
+++ problem7.rb
@@ -1,32 +1,36 @@
 class Writer
+  attr_reader :artist
   def initialize(artist)
     @artist = artist
   end
 
   def create
-    puts "This is a write"
+    puts "#{@artist} is a writer"
   end
 end
 
 
 class Painter
+  attr_reader :artist
   def initialize(artist)
   @artist = artist
   end
 
   def create
-    puts "This is a painter"
+    puts "#{@artist} is a painter"
   end
 end
+
 writer_1 = Writer.new('Writer 1')
 painter_1 = Painter.new('Painter 1')
 
 array_1 = [writer_1, painter_1]
-def show_case_talent(array)
-  array.each do |ele|
-    puts ele.create
+
+def show_case_talent(array1)
+  array1.each do |ele|
+    ele.create
   end
 end
 
 
-puts show_case_talent(array_1)
+show_case_talent(array_1)
